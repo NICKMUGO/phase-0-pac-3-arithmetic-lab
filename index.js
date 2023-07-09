1122e7877c02cb9c8bca7a9c72b6922a38fdc430
NICK MUGO NJURU <nicknjuru13@gmail.com>
2023-07-09 10:52:37 +0300

    Completed assignmnet

---
+++ index.js
@@ -0,0 +1,15 @@
+function add(number){
+    return number+number;
+}
+console.log(add(728+3316));
+function subtract(number){
+    return number-number;
+}
+function multiply(number){
+    return number*number;
+}
+function divide(number){
+    return number/number;
+}
+
+
